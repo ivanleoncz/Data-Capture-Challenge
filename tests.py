2452b5986d489722eeb5cf0bbd2e3933c3bf88b3
ivanleoncz <ivanlmj@gmail.com>
2024-01-06 22:09:24 -0600

    fix: return integers rather than lists
    The entire logic of build_stats() was refactored, incrementing values
    instead of appending numbers into lists, in order to return the expected
    results for less, greater and between methods, the later being
    refactored as well, in order to ensure that its time complexity
    is not O(n): could be O(1) or in its worst case, O(2).

--- tests.py
+++ tests.py
@@ -10,21 +10,21 @@ class TestDataCapture(unittest.TestCase):
 
     def test_attributes(self):
         """
-        > Expected results: attributes must be present and if they are variables, they must be of certain types.
+        > Expected attributes and if they are variables, they must be of certain types.
         """
-        self.assertTrue(hasattr(self.dc, "add"))
-        self.assertTrue(hasattr(self.dc, "between"))
-        self.assertTrue(hasattr(self.dc, "build_stats"))
-        self.assertTrue(hasattr(self.dc, "less"))
-        self.assertTrue(hasattr(self.dc, "greater"))
-        self.assertTrue(hasattr(self.dc, "numbers"))
-        self.assertTrue(isinstance(self.dc.numbers, list))
-        self.assertTrue(hasattr(self.dc, "stats"))
-        self.assertTrue(isinstance(self.dc.stats, dict))  # 'dict' is the parent class of 'defaultdict'
+        self.assertTrue(hasattr(self.dc, "add"), msg="add() method is missing")
+        self.assertTrue(hasattr(self.dc, "between"), msg="between() method is missing")
+        self.assertTrue(hasattr(self.dc, "build_stats"), msg="build_stats() method is missing")
+        self.assertTrue(hasattr(self.dc, "less"), msg="less() method is missing")
+        self.assertTrue(hasattr(self.dc, "greater"), msg="greater() method is missing")
+        self.assertTrue(hasattr(self.dc, "numbers"), msg="numbers variable is missing")
+        self.assertTrue(isinstance(self.dc.numbers, list), msg="numbers must be of type 'list'")
+        self.assertTrue(hasattr(self.dc, "stats"), msg="stats variable is missing")
+        self.assertTrue(isinstance(self.dc.stats, dict), msg="numbers must be of type 'dict'")
 
     def test_input_validation(self):
         """
-        > Expected results: all mentioned exceptions must be successfully raised.
+        > Expected exceptions must be raised.
         """
         self.assertRaises(TypeError, self.dc.build_stats)
         self.assertRaises(TypeError, self.dc.add, "0")
@@ -36,11 +36,12 @@ class TestDataCapture(unittest.TestCase):
         self.dc.add(9)
         self.dc.add(1)
         self.assertRaises(TypeError, self.dc.less, "5")
-        self.assertRaises(ValueError, self.dc.less, 5)
         self.assertRaises(TypeError, self.dc.greater, "2")
-        self.assertRaises(ValueError, self.dc.greater, 2)
         self.assertRaises(TypeError, self.dc.between, "1", 5)
+        self.assertRaises(ValueError, self.dc.less, 5)
+        self.assertRaises(ValueError, self.dc.greater, 2)
         self.assertRaises(ValueError, self.dc.between, 1, 5)
+        self.assertRaises(ValueError, self.dc.between, 9, 1)
 
     def test_less_with_one_number(self):
         """
@@ -49,7 +50,8 @@ class TestDataCapture(unittest.TestCase):
         x = 2
         self.dc.add(x)
         self.stats = self.dc.build_stats()
-        self.assertEqual(first=self.stats.less(x=x), second=[])
+        self.assertEqual(first=self.stats.less(x=x), second=0,
+                         msg=f"there must be no value lesser than {x}, since it's the only value processed")
 
     def test_less_with_five_numbers(self):
         """
@@ -59,7 +61,8 @@ class TestDataCapture(unittest.TestCase):
         for n in (3, 9, 7, 5):
             self.dc.add(n)
         self.stats = self.dc.build_stats()
-        self.assertEqual(first=self.stats.less(x=x), second=[3, 5])
+        self.assertEqual(first=self.stats.less(x=x), second=2,
+                         msg=f"amount of numbers lesser than {x}, surpasses the expected amount")
 
     def test_less_with_ten_numbers(self):
         """
@@ -69,7 +72,8 @@ class TestDataCapture(unittest.TestCase):
         for n in (2, 1, 1, 3, 9, 5, 2, 6, 4):
             self.dc.add(n)
         self.stats = self.dc.build_stats()
-        self.assertEqual(first=self.stats.less(x=x), second=[1, 1, 2, 2, 3, 4, 5])
+        self.assertEqual(first=self.stats.less(x=x), second=7,
+                         msg=f"amount of numbers lesser than {x}, surpasses the expected amount")
 
     def test_greater_with_one_number(self):
         """
@@ -78,7 +82,7 @@ class TestDataCapture(unittest.TestCase):
         x = 4
         self.dc.add(x)
         self.stats = self.dc.build_stats()
-        self.assertEqual(first=self.stats.greater(x=x), second=[])
+        self.assertEqual(first=self.stats.greater(x=x), second=0)
 
     def test_greater_with_five_numbers(self):
         """
@@ -88,7 +92,8 @@ class TestDataCapture(unittest.TestCase):
         for n in (2, 1, 8, 5, 8):
             self.dc.add(n)
         self.stats = self.dc.build_stats()
-        self.assertEqual(first=self.stats.greater(x=x), second=[5, 8, 8])
+        self.assertEqual(first=self.stats.greater(x=x), second=3,
+                         msg=f"amount of numbers greater than {x}, surpasses the expected amount")
 
     def test_greater_with_ten_numbers(self):
         """
@@ -98,25 +103,32 @@ class TestDataCapture(unittest.TestCase):
         for n in (5, 2, 9, 3, 3, 7, 1, 2, 5, 6):
             self.dc.add(n)
         self.stats = self.dc.build_stats()
-        self.assertEqual(first=self.stats.greater(x=x), second=[5, 5, 6, 7, 9])
+        self.assertEqual(first=self.stats.greater(x=x), second=5,
+                         msg=f"amount of numbers greater than {x}, surpasses the expected amount")
 
     def test_between_with_seven_numbers(self):
         """
         > Expected result: list of positive numbers between "start" and "end"
         """
+        start, end = 4, 9
         for n in (4, 3, 6, 4, 12, 6, 9):
             self.dc.add(n)
         self.stats = self.dc.build_stats()
-        self.assertEqual(first=self.stats.between(start=4, end=9), second=[4, 4, 6, 6, 9])
+        result, amount_expected = self.stats.between(start=start, end=end), 5
+        self.assertEqual(first=result, second=amount_expected,
+                         msg=f"result: {result} / amount expected: {amount_expected}")
 
     def test_between_with_ten_numbers(self):
         """
         > Expected result: list of positive numbers between "start" and "end"
         """
+        start, end = 3, 9
         for n in (3, 9, 5, 5, 5, 12, 5, 6, 1, 25):
             self.dc.add(n)
         self.stats = self.dc.build_stats()
-        self.assertEqual(first=self.stats.between(start=3, end=9), second=[3, 5, 5, 5, 5, 6, 9])
+        result, amount_expected = self.stats.between(start=start, end=end), 7
+        self.assertEqual(first=result, second=amount_expected,
+                         msg=f"result: {result} / amount expected: {amount_expected}")
 
 
 if __name__ == '__main__':
